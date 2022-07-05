a7408abe189a8ac5b270373303cdb7343d7b7cd5
Kent Michael San <kentmichaelsan22@gmail.com>
2022-07-06 01:00:24 +0800

    Create search and filter functionality

--- src/pages/main/components/search-filter-container/components/filter/index.jsx
+++ src/pages/main/components/search-filter-container/components/filter/index.jsx
@@ -1,7 +1,20 @@
-import React from "react"
+import React, { useContext } from "react"
+import { AppContext } from "@/setup/app-context-manager/appContext"
 
 const Filter = () => {
-  return <button>Filter By</button>
+  const { stateFilter, dispatchFilter } = useContext(AppContext)
+
+  const handleOptionChange = (e) => {
+    dispatchFilter({ type: "FILTER_OPTION", payload: e.target.value })
+  }
+
+  return (
+    <select value={stateFilter.filterOption} onChange={handleOptionChange}>
+      <option value="All">All</option>
+      <option value="Active">Active</option>
+      <option value="Completed">Completed</option>
+    </select>
+  )
 }
 
 export default Filter
--- src/pages/main/components/search-filter-container/components/search/index.jsx
+++ src/pages/main/components/search-filter-container/components/search/index.jsx
@@ -1,7 +1,27 @@
-import React from "react"
+import React, { useContext, useState, useTransition } from "react"
+import { AppContext } from "@/setup/app-context-manager/appContext"
 
 const Search = () => {
-  return <input type="search" />
+  const [isPending, startTransition] = useTransition()
+  const [input, setInput] = useState("")
+  const { dispatchFilter } = useContext(AppContext)
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value)
+
+    startTransition(() => {
+      dispatchFilter({ type: "SEARCH_KEYWORD", payload: e.target.value })
+    })
+  }
+
+  return (
+    <input
+      type="search"
+      value={input}
+      onChange={handleInputChange}
+      placeholder="Search item..."
+    />
+  )
 }
 
 export default Search
