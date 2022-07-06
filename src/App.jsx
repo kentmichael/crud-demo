8c5e44edaacbf9ee9c5bd364f4281ae1b00e36bc
Kent Michael San <kentmichaelsan22@gmail.com>
2022-07-06 10:09:17 +0800

    Clean files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react"
+import AddMarkContainer from "./pages/main/components/add-mark-container"
 import SearchAndFilterContainer from "./pages/main/components/search-filter-container"
-import TodoList from "./pages/main/components/todo-list"
+import TodoList from "./pages/main/components/todo-list-table"
+import AppContextProvider from "./setup/app-context-manager/appContext"
+import DialogModal from "./common/modal/components"
 
 const App = () => {
   return (
     <>
-      <h1>CRUD Demo</h1>
-      <SearchAndFilterContainer />
-      <TodoList />
+      <AppContextProvider>
+        <h1>CRUD Demo</h1>
+        <SearchAndFilterContainer />
+        <TodoList />
+        <AddMarkContainer />
+        <DialogModal />
+      </AppContextProvider>
     </>
   )
 }
